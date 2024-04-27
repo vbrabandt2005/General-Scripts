9530f116ac07b1962c9fa218d209543b18cf7cd3
vbrabandt2005 <vbrabandt@proton.me>
2024-04-27 13:20:38 +0200

    Update UpdateMe.sh
    Whats been done:
    - added description
    - updated texts
    - replaced echo with printf (because more flexible)
    - improved unnecessary eos-update with paru and yay checks
    - improved arch linux update checks with paru and yay checks
    - made snap refresh run in sudo, just because if you do apt, you need sudo anyways, it would be annoying to write password twice
    
    Whats need to be improved:
    -  Improve formatting

--- UpdateMe/UpdateMe.sh
+++ UpdateMe/UpdateMe.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# UpdateMe.sh is a simple shell script which detects what OS
+# the script is running on and run the appropriate update command for that system
+# then proceeds to check if snap, flatpak or Pacstall is installed
+# and run the commands to check for updates for those too
+#
+# By vbrabandt2005 <vbrabandt@proton.me>
+
 # Check for compatible OS file
 if [ ! -f /etc/os-release ]; then
-  echo "Error: This script is only for systems with /etc/os-release file."
+  printf "Error: This script seems to not be supported by this script.\n"
+  #"Error: This script is only for systems with /etc/os-release file."
   exit 1
 fi
 
@@ -11,56 +19,71 @@ os_name=$(grep -Eo '^ID=.*' /etc/os-release | cut -d= -f2- | tr -d '"')
 
 # Update for Debian-based systems (including nala-apt check)
 if command -v dpkg >/dev/null 2>&1; then
-  # Likely a Debian-based system, use apt commands (adjust as needed)
-  echo "Detected Debian-based system, using apt..."
+  # Likely a Debian-based system, use apt (or nala) commands
+  printf "Detected Debian-based system\n"
   if command -v nala >/dev/null 2>&1; then
     # nala-apt exists, use nala commands
-    echo "Using nala for update and upgrade..."
+    printf "nala found, using nala to update...\n"
     sudo nala update && sudo nala full-upgrade
   else
     # nala-apt not found, use apt commands
-    echo "nala-apt not found, using apt..."
+    printf "nala not found, using apt to update...\n"
     sudo apt update && sudo apt full-upgrade
   fi
   
   # Check for and update with pactall (if installed)
   if command -v pacstall >/dev/null 2>&1; then
-    echo "pactall is installed, will check for pactall updates..."
+    printf "pactall is installed, will check for pactall updates...\n"
     pacstall -U && pacstall -Up
   fi
 fi
 
-# Update EndeavourOS
+# Update EndeavourOS 
+# (honestly idk why I have this, considering eos-update is just a frontend for pacman, yay & paru)
 if [[ "$os_name" == endeavouros ]]; then
-  echo "Detected EndeavourOS, updating..."
-  eos-update --paru
+  if command -v paru >/dev/null 2>&1; then
+    argu=--paru
+    endy=paru
+  elif command -v yay >/dev/null 2>&1; then
+    argu=--yay
+    endy=yay
+  fi
+  printf "Detected EndeavourOS, updating with eos-update with %s$endy...\n"
+  eos-update "$argu"
 fi
 
-# Update Arch Linux (including yay check)
+# Update Arch Linux (including yay & paru check)
 if [[ "$os_name" == arch ]]; then
-  if command -v yay >/dev/null 2>&1; then
-    echo "Detected Arch Linux, updating with yay..."
-    yay -Syu --devel
+  if command -v paru >/dev/null 2>&1; then
+    printf "Detected Arch Linux, updating with paru..."
+    paru
+  elif command -v yay >/dev/null 2>&1; then
+    printf "Detected Arch Linux, updating with yay..."
+    yay
   else
-    echo "Detected Arch Linux, updating with pacman..."
-    sudo pacman -Syyu
+    printf "Detected Arch Linux, unable to find paru or yay, updating with pacman..."
+    sudo pacman -Syu
   fi
 fi
 
+echo
+
 # Update snaps (if installed)
 if command -v snap >/dev/null 2>&1; then
-  echo "snap is installed, will check for snap updates..."
-  snap refresh
+  printf "Snap is installed, will check for snap updates...\n"
+  sudo snap refresh
 else
-  echo "snap is not found, will skip checking snap updates."
+  printf "Snap is not found, will skip checking snap updates."
 fi
+
+echo
 
 # Update flatpaks (if installed)
 if command -v flatpak >/dev/null 2>&1; then
-  echo "flatpak is installed, will check for flatpak updates..."
+  printf "Flatpak is installed, will check for flatpak updates...\n"
   flatpak update
 else
-  echo "flatpak is not found, will skip checking flatpak updates."
+  printf "Flatpak is not found, will skip checking flatpak updates.\n"
 fi
 
-echo "All update checks complete."
+printf "All update checks complete."
